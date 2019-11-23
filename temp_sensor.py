dd24d6b9a0d002e85d85187d7042972090d41f2d
Arjun BJ <47909735+Arjun-B-J@users.noreply.github.com>
2019-11-23 11:25:48 +0530

    Create temp_sensor.py

---
+++ temp_sensor.py
@@ -0,0 +1,24 @@
+import board
+import analogio
+import time
+ 
+ 
+TMP36_PIN = board.A0  # Analog input connected to TMP36 output.
+ 
+ 
+# Function to simplify the math of reading the temperature.
+def tmp36_temperature_C(analogin):
+    millivolts = analogin.value * (analogin.reference_voltage * 1000 / 65535)
+    return (millivolts - 500) / 10
+ 
+tmp36 = analogio.AnalogIn(TMP36_PIN)
+ 
+# Loop forever.
+while True:
+    # Read the temperature in Celsius.
+    temp_C = tmp36_temperature_C(tmp36)
+    # Convert to Fahrenheit.
+    temp_F = (temp_C * 9/5) + 32
+    # Print out the value and delay a second before looping again.
+    print("Temperature: {}C {}F".format(temp_C, temp_F))
+    time.sleep(1.0)
